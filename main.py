4b431f776bc8c3e42c87f07ff2c9cfa568c3f816
adamd1116 <adamdunleavy15@gmail.com>
2021-10-23 11:04:19 +0100

    made algorithm to only show first letter of each word

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import random
 password = input("\nEnter your password: ")
 
 songs = {
-    "kanye": "Off the Grid",
+    "Kanye": "Off The Grid",
+    "Future": "Life Is Good",
 }
 
-song_artist = ['kanye']
+song_artist = list(songs.keys())
+song_names = list(songs.values())
 
 def rand_selection(x):
     return random.choice(x)
@@ -20,4 +22,24 @@ with open('password.txt', 'r') as f:
         passw_correct = False
 
 if passw_correct == True:
-    chosen 
+    pass
+
+choice1 = str(song_names[1])
+choice1_m = list(choice1)
+b = 0
+
+
+for i in range(len(choice1_m)):
+    if i == 0:
+        pass
+    else:
+        choiceindex = choice1.find(choice1[i])
+        if choice1[i] != " " and choice1[i].isupper() != True:
+            choice1_m[choiceindex] = "_"
+            if i < len(choice1_m)-1 and choice1[i] == choice1[i+1]:
+                choice1_m[choiceindex+1] = "_"
+        else:
+            choice1_m[choiceindex] = choice1[i]
+
+e = "".join(choice1_m)
+print(e)
